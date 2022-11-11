358f31b4a4b8f5fe6a04b1dd84c20a2656d256d2
ismail <ismailtoyran@gmail.com>
2022-11-11 13:40:19 +0300

    Change / Styling changes to headings

--- src/components/ConversationsList/NoConversationsMessage.tsx
+++ src/components/ConversationsList/NoConversationsMessage.tsx
@@ -10,10 +10,10 @@ const NoConversationsMessage = (): JSX.Element => {
       textAlign="center"
       p={4}
     >
-      <Heading size="md" fontWeight="600" marginY="4">
+      <Heading fontSize="md" fontWeight="600" marginY="4">
         Your message list is empty
       </Heading>
-      <Heading size="sm" fontWeight="500">
+      <Heading fontSize="sm" fontWeight="500">
         There are no messages for this address
       </Heading>
     </Flex>
